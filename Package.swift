8e9ab88d55244ce491403c5cae47b56e9c2980d6
Navneet Gupta <navneet@us.ibm.com>
2017-01-16 15:51:49 -0600

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -18,5 +18,5 @@ import PackageDescription
 
 let package = Package(
     name: "KituraStencil",
-    dependencies: [.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 4),
+    dependencies: [.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 5),
                    .Package(url: "https://github.com/kylef/Stencil", majorVersion: 0, minor: 7)])
